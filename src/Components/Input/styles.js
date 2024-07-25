fa333a5710b60baed2d88e98c4a9f215b11ced66
André Carlos Viana <andre.carlos.viana@gmail.com>
2024-07-25 11:38:20 -0300

    Desafio calculadora DIO

---
+++ src/Components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div `
+    width: 100%;
+    heigth: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        float: right;
+        display: flex;
+        flex-direction: column;
+        clear: both;
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+        color: #FFFFFF;
+    }
+
+`
+
